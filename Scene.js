1f176160f1b763e9487a6f5ed6fff5a1acf6eeab
matyi <matyi204@gmail.com>
2015-05-19 13:17:07 +0200

    added form

--- Scene.js
+++ Scene.js
@@ -12,21 +12,29 @@ var graphx;
 
 function init()
 {
-    phx = new Physics(new Vector(0.0,0.01),0.3,500,500);
+    phx = new Physics(new Vector(0.0,0.01),0.4,500,500);
     graphx = new Graphics("myCanvas");
     
     var circle = new Circle(20, 5, new Vector(150,50), new Vector(0,0), new Vector(0,0));
 	circles.push(circle); 
-    
-    circle = new Circle(20, 5, new Vector(120,150), new Vector(0,0), new Vector(0,0));
-	circles.push(circle); 
-    
-    circle = new Circle(20, 3, new Vector(40,150), new Vector(0,0), new Vector(0,0));
-	circles.push(circle); 
+
 }
  
 function run() {
     phx.updateObjects(circles, Date.now());
     graphx.drawObjects(circles);
 	requestAnimationFrame(run);
+}
+
+function addCircle()
+{
+    var mass = parseFloat(document.getElementById('mass').value);
+    var radius = parseFloat(document.getElementById('radius').value);
+    var xPos = parseFloat(document.getElementById('xPos').value);
+    var yPos = parseFloat(document.getElementById('yPos').value);
+    var xVel = parseFloat(document.getElementById('xVel').value);
+    var yVel = parseFloat(document.getElementById('yVel').value);
+   
+    var circle = new Circle(mass, radius, new Vector(xPos,yPos), new Vector(xVel,yVel), new Vector(0,0)); 
+    circles.push(circle);
 }
